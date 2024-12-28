e7360f6eb5b0ee4cb591ecb97ca713d19527a49f
Asadbek <asadbeeksafaraliyev@gmail.com>
2024-12-29 03:54:20 +0800

    Created pages

---
+++ client/src/pages/SignUp.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function SignUp() {
+  return (
+    <div className='bg-sky-400'>
+      
+      <div className='text-red-600'>SignUp</div>
+      <div className="text-yellow-400 text-bold">Test Yellow 400</div>
+<div className="text-yellow-700">Test Yellow 700</div>
+<div className="text-[yellow]">Custom Yellow</div>
+
+    </div>
+    
+  )
+}
