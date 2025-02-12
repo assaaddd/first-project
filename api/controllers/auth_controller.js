28a5c4b2f039ee92e7640475dc94199af1fc2b55
Asadbek <asadbeeksafaraliyev@gmail.com>
2025-02-13 07:22:11 +0800

    create a middleware and a function to handle possible errors

--- api/controllers/auth_controller.js
+++ api/controllers/auth_controller.js
@@ -1,4 +1,5 @@
 import bcryptjs from 'bcryptjs';
+
 import User from "../models/User.js";
 
 export const signUp = async (req, res, next) => {
@@ -9,6 +10,6 @@ export const signUp = async (req, res, next) => {
         await newUser.save();
         res.status(201).json(newUser);
     }catch(err){
-        res.status(500).json(`Unexpected error in sign up: ${err}`);
+        next(err);
     }
 }
--- api/index.js
+++ api/index.js
@@ -23,3 +23,13 @@ app.listen(PORT, () => {
         console.log('Error in app listen');
     }
 });
+
+app.use((err , req , res , next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal server error";
+    return res.status(statusCode).json({
+        success:false,
+        statusCode,
+        message
+    })
+});
